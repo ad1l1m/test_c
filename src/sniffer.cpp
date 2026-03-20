c2caf9cc3fa7e67eacd1e8b3f04e1b0f4684d77f
ad1l <adil.alimbaev.06@gmail.com>
2026-03-21 01:47:27 +0500

    test2

--- src/sniffer.cpp
+++ src/sniffer.cpp
@@ -1,8 +1,81 @@
 #include <pcap.h>
 #include <iostream>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+struct ethernet_header {
+    u_char dest[6];
+    u_char src[6];
+    u_short type;
+};
+
+struct ip_header {
+    u_char ver_ihl;      // версия + длина заголовка
+    u_char tos;
+    u_short tlen;
+    u_short identification;
+    u_short flags_fo;
+    u_char ttl;
+    u_char proto;
+    u_short crc;
+    u_int saddr;
+    u_int daddr;
+};
+
+struct tcp_header {
+    u_short source;
+    u_short dest;
+    u_int seq;
+    u_int ack_seq;
+    u_char doff_reserved;
+    u_char flags;
+    u_short window;
+    u_short check;
+    u_short urg_ptr;
+};
 
 void packet_handler(u_char *args, const struct pcap_pkthdr *header, const u_char *packet) {
-    std::cout << "Packet captured: length = " << header->len << std::endl;
+    // 1. Ethernet
+    const ethernet_header *eth = (ethernet_header *)packet;
+
+    // проверка: это IPv4?
+    if (ntohs(eth->type) != 0x0800) {
+        return;
+    }
+
+    // 2. IP header
+    const ip_header *ip = (ip_header *)(packet + sizeof(ethernet_header));
+
+    int ip_header_length = (ip->ver_ihl & 0x0F) * 4;
+
+    // IP адреса
+    struct in_addr src, dst;
+    src.s_addr = ip->saddr;
+    dst.s_addr = ip->daddr;
+
+    std::cout << inet_ntoa(src) << " -> " << inet_ntoa(dst);
+
+    // 3. TCP
+    if (ip->proto == IPPROTO_TCP) {
+
+        const tcp_header *tcp = (tcp_header *)(
+            packet + sizeof(ethernet_header) + ip_header_length
+        );
+
+        std::cout << " | TCP ";
+
+        std::cout << ntohs(tcp->source) << " -> " << ntohs(tcp->dest);
+
+    }
+
+    // 4. UDP
+    else if (ip->proto == IPPROTO_UDP) {
+
+        std::cout << " | UDP";
+
+    }
+
+    std::cout << std::endl;
 }
 
 void start_sniffing() {
